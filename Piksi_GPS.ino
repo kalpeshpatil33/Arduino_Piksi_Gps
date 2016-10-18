d31727e2de8dde5a22e1eb4346fa8a51677b2c64
kalpesh <kalpeshpatil33@gwu.edu>
2016-10-18 14:31:14 -0400

    Started with llh message

--- Piksi_GPS.ino
+++ Piksi_GPS.ino
@@ -46,6 +46,10 @@ void processIncomingByte (const byte inByte)
       {
         vel_msg_analyse (input_msg);
       }
+      if ((input_msg[0] == 0x01) && (input_msg[1] == 0x02))
+      {
+        llh_msg_analyse (input_msg);
+      }
     }
     input_pos = 0;
   }
@@ -57,7 +61,10 @@ void processIncomingByte (const byte inByte)
       input_pos = input_pos + 1;
     }
   }
+}
 
+void llh_msg_analyse (byte byte_msg[41])
+{
 }
 
 void pos_msg_analyse (byte byte_msg[29])
@@ -68,27 +75,27 @@ void pos_msg_analyse (byte byte_msg[29])
 
   double pos_north = 0;
   pos_north = bytesToInt(byte_msg[12], byte_msg[11], byte_msg[10], byte_msg[9]);
-//  Serial.print(pos_north); Serial.print("  ");
+  //  Serial.print(pos_north); Serial.print("  ");
 
   double pos_east = 0;
   pos_east = bytesToInt(byte_msg[16], byte_msg[15], byte_msg[14], byte_msg[13]);
-//  Serial.print(pos_east); Serial.print("  ");
+  //  Serial.print(pos_east); Serial.print("  ");
 
   double pos_down = 0;
   pos_down = bytesToInt(byte_msg[20], byte_msg[19], byte_msg[18], byte_msg[17]);
-//  Serial.print(pos_down); Serial.print("  ");
+  //  Serial.print(pos_down); Serial.print("  ");
 
   double pos_hacc = 0;
   pos_hacc = Bytes2Intu16(byte_msg[22], byte_msg[21]);
-//  Serial.print(pos_hacc); Serial.print("  ");
+  //  Serial.print(pos_hacc); Serial.print("  ");
 
   double pos_vacc = 0;
   pos_vacc = Bytes2Intu16(byte_msg[24], byte_msg[23]);
-//  Serial.print(pos_vacc); Serial.print("  ");
+  //  Serial.print(pos_vacc); Serial.print("  ");
 
   double distance = 0;
   distance = sqrt(pos_north * pos_north + pos_east * pos_east);
-//  Serial.print(distance); Serial.print("  ");
+  //  Serial.print(distance); Serial.print("  ");
 
   line1 = String(pos_ntime) + "," + String(pos_north) + "," + String(pos_east) + "," + String(pos_down) + "," + String(pos_hacc) + "," + String(pos_vacc) + "," + String(distance) + "," ;
 }
@@ -97,27 +104,27 @@ void vel_msg_analyse (byte byte_msg[29])
 {
   double vel_ntime = 0;
   vel_ntime = bytesToInt(byte_msg[8], byte_msg[7], byte_msg[6], byte_msg[5]);
-//  Serial.print(vel_ntime); Serial.print("  ");
+  //  Serial.print(vel_ntime); Serial.print("  ");
 
   double vel_north = 0;
   vel_north = bytesToInt(byte_msg[12], byte_msg[11], byte_msg[10], byte_msg[9]);
-//  Serial.print(vel_north); Serial.print("  ");
+  //  Serial.print(vel_north); Serial.print("  ");
 
   double vel_east = 0;
   vel_east = bytesToInt(byte_msg[16], byte_msg[15], byte_msg[14], byte_msg[13]);
-//  Serial.print(vel_east); Serial.print("  ");
+  //  Serial.print(vel_east); Serial.print("  ");
 
   double vel_down = 0;
   vel_down = bytesToInt(byte_msg[20], byte_msg[19], byte_msg[18], byte_msg[17]);
-//  Serial.print(vel_down); Serial.print("  ");
+  //  Serial.print(vel_down); Serial.print("  ");
 
   double vel_hacc = 0;
   vel_hacc = Bytes2Intu16(byte_msg[22], byte_msg[21]);
-//  Serial.print(vel_hacc); Serial.print("  ");
+  //  Serial.print(vel_hacc); Serial.print("  ");
 
   double vel_vacc = 0;
   vel_vacc = Bytes2Intu16(byte_msg[24], byte_msg[23]);
-//  Serial.print(vel_vacc); Serial.println("  ");
+  //  Serial.print(vel_vacc); Serial.println("  ");
 
   line2 = line1 + String(vel_ntime) + "," + String(vel_north) + "," + String(vel_east) + "," + String(vel_down) + "," + String(vel_hacc) + "," + String(vel_vacc) + ",";
   Serial.println(line2);
